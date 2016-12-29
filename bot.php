6da4907eaf968fb4c00a7339decc9b3b4eec668a
wirun <moynaja@gmail.com>
2016-12-29 15:52:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,15 +12,29 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
+			
+		
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			
+			if($text == 'มอย'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'Bot มอยโคตรเจ๋ง!!!'
+				];
+				
+			}else{
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+			}
+		
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
